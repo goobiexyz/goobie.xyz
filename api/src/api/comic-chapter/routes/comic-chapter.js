3a4ce52cadfe8263ca0dce1d6f6329b6e946565c
Gracie Bell <daineesvang@gmail.com>
2023-02-26 21:01:15 +0000

    added api comics collection to main repo

---
+++ api/src/api/comic-chapter/routes/comic-chapter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * comic-chapter router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::comic-chapter.comic-chapter');
---
+++ api/src/api/comic-chapter/services/comic-chapter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * comic-chapter service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::comic-chapter.comic-chapter');
