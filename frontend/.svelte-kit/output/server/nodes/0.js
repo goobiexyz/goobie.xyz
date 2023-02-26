f98ee95016be3f90ec64af5b7d6744b7c15562d0
Gracie Bell <daineesvang@gmail.com>
2023-02-26 21:31:26 +0000

    removed .svelte-kit directory from being tracked

--- frontend/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
-export const file = '_app/immutable/entry/_layout.svelte.6457fdb8.js';
-export const imports = ["_app/immutable/entry/_layout.svelte.6457fdb8.js","_app/immutable/chunks/index.f91f41ce.js"];
-export const stylesheets = ["_app/immutable/assets/_layout.657fc4e3.css"];
-export const fonts = [];
