722e64fbe06c90819e54d0200382ef1a07927d71
Gracie Bell <graciebellart@gmail.com>
2023-02-26 12:50:14 -0600

    added adapter to config file

--- frontend/.svelte-kit/output/server/nodes/1.js
+++ frontend/.svelte-kit/output/server/nodes/1.js
@@ -2,7 +2,7 @@
 
 export const index = 1;
 export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/entry/error.svelte.5539cda9.js';
-export const imports = ["_app/immutable/entry/error.svelte.5539cda9.js","_app/immutable/chunks/index.f91f41ce.js","_app/immutable/chunks/stores.87d077fd.js","_app/immutable/chunks/singletons.99ab03bd.js"];
+export const file = '_app/immutable/entry/error.svelte.15f64049.js';
+export const imports = ["_app/immutable/entry/error.svelte.15f64049.js","_app/immutable/chunks/index.f91f41ce.js","_app/immutable/chunks/stores.f497b55b.js","_app/immutable/chunks/singletons.423f4a06.js"];
 export const stylesheets = [];
 export const fonts = [];
--- frontend/.svelte-kit/output/server/nodes/6.js
+++ frontend/.svelte-kit/output/server/nodes/6.js
@@ -2,9 +2,9 @@ import * as server from '../entries/pages/comics/_comic_/_chapter_/_page.server.
 
 export const index = 6;
 export const component = async () => (await import('../entries/pages/comics/_comic_/_chapter_/_page.svelte.js')).default;
-export const file = '_app/immutable/entry/comics-_comic_-_chapter_-page.svelte.c355fe87.js';
+export const file = '_app/immutable/entry/comics-_comic_-_chapter_-page.svelte.9ec5ff7b.js';
 export { server };
 export const server_id = "src/routes/comics/[comic]/[chapter]/+page.server.ts";
-export const imports = ["_app/immutable/entry/comics-_comic_-_chapter_-page.svelte.c355fe87.js","_app/immutable/chunks/index.f91f41ce.js","_app/immutable/chunks/Page.4488cce5.js","_app/immutable/chunks/Image.447b0612.js","_app/immutable/chunks/stores.87d077fd.js","_app/immutable/chunks/singletons.99ab03bd.js"];
+export const imports = ["_app/immutable/entry/comics-_comic_-_chapter_-page.svelte.9ec5ff7b.js","_app/immutable/chunks/index.f91f41ce.js","_app/immutable/chunks/Page.4488cce5.js","_app/immutable/chunks/Image.447b0612.js","_app/immutable/chunks/stores.f497b55b.js","_app/immutable/chunks/singletons.423f4a06.js"];
 export const stylesheets = ["_app/immutable/assets/_page.3e4e08d2.css","_app/immutable/assets/Page.9f350518.css","_app/immutable/assets/Image.5ec57a55.css"];
 export const fonts = [];
