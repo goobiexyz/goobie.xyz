4845c71fec19bc5f373a431fcb13c7e2d34f7d45
goobie <daineesvang@gmail.com>
2024-03-26 20:03:28 -0500

    new component, Card

---
+++ src/lib/components/containers/Card.svelte
@@ -0,0 +1,23 @@
+<script>
+</script>
+  
+
+<div>
+    <slot />
+</div>
+
+
+<style lang="postcss">
+    div {
+        background: white;
+        border-radius: 1em;
+        padding: 2em;
+    }
+
+    @media only screen and (max-width: token(breakpoints.ss)) {
+    div {
+        padding: 1em;
+    }
+  }
+</style>
+  
